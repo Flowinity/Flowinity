368557261ba5a67f9fcc5ee34657b17b0e3ad7da
Troplo <troplo@troplo.com>
2023-01-18 14:47:06 +1100

    Create 20230117082353-pinnedBetter.js

---
+++ app/migrations/20230117082353-pinnedBetter.js
@@ -0,0 +1,21 @@
+"use strict"
+
+/** @type {import("sequelize-cli").Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("CollectionItems", "pinned", {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+}
