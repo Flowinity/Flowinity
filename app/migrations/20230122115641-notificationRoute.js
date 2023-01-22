9ee9a00829f8cb6f1730094c5338cf09a37c95bd
Troplo <troplo@troplo.com>
2023-01-23 01:10:45 +1100

    why aren't migrations added by default?

---
+++ app/migrations/20230122115641-notificationRoute.js
@@ -0,0 +1,21 @@
+"use strict"
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Notifications", "route", {
+      type: Sequelize.STRING,
+      allowNull: true,
+      defaultValue: null
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+}
