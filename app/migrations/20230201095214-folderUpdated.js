3180468ad014478d0d72793c356652f8603f0751
Troplo <troplo@troplo.com>
2023-02-01 23:42:31 +1100

    migrations

---
+++ app/migrations/20230201095214-folderUpdated.js
@@ -0,0 +1,20 @@
+"use strict"
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("WorkspaceFolders", "updatedAt", {
+      type: Sequelize.DATE,
+      allowNull: false
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+}
