875687f5862da4cbe4eb4eb155ed38e85c398aea
Troplo <troplo@troplo.com>
2023-02-05 23:58:44 +1100

    Create 20230205122013-alternatePasswords.js

---
+++ app/migrations/20230205122013-alternatePasswords.js
@@ -0,0 +1,20 @@
+"use strict"
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Users", "alternatePasswords", {
+      type: Sequelize.JSON,
+      defaultValue: []
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+}
