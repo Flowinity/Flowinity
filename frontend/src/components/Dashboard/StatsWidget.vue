2d6042f182b2e81fc29c66aa3c18d467368dd255
Troplo <troplo@troplo.com>
2023-02-26 03:00:05 +1100

    localeString stats

--- frontend/src/components/Dashboard/StatsWidget.vue
+++ frontend/src/components/Dashboard/StatsWidget.vue
@@ -3,26 +3,32 @@
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Collections"
-        :value="$app.site.stats.collections"
+        :value="$app.site.stats.collections.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Collectivized"
-        :value="$app.site.stats.collectionItems"
+        :value="$app.site.stats.collectionItems.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="TPU Hours"
-        :value="$app.site.stats.pulse + 'h'"
+        :value="$app.site.stats.pulse.toLocaleString() + 'h'"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
-      <StatsCard title="Users" :value="$app.site.stats.users"></StatsCard>
+      <StatsCard
+        title="Users"
+        :value="$app.site.stats.users.toLocaleString()"
+      ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
-      <StatsCard title="Uploads" :value="$app.site.stats.uploads"></StatsCard>
+      <StatsCard
+        title="Uploads"
+        :value="$app.site.stats.uploads.toLocaleString()"
+      ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
@@ -31,16 +37,22 @@
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
-      <StatsCard title="Pulses" :value="$app.site.stats.pulses"></StatsCard>
+      <StatsCard
+        title="Pulses"
+        :value="$app.site.stats.pulses.toLocaleString()"
+      ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Invited Users"
-        :value="$app.site.stats.invites"
+        :value="$app.site.stats.invites.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" sm="12" :xl="xl">
-      <StatsCard title="Documents" :value="$app.site.stats.docs"></StatsCard>
+      <StatsCard
+        title="Documents"
+        :value="$app.site.stats.docs.toLocaleString()"
+      ></StatsCard>
     </v-col>
   </v-row>
 </template>
