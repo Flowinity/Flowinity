dfc9c85b4387aa79b58de81bbf998249bc16075c
Troplo <troplo@troplo.com>
2023-02-27 21:23:36 +1100

    Update StatsWidget.vue

--- frontend/src/components/Dashboard/StatsWidget.vue
+++ frontend/src/components/Dashboard/StatsWidget.vue
@@ -3,31 +3,31 @@
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Collections"
-        :value="$app.site.stats.collections.toLocaleString()"
+        :value="$app.site.stats.collections?.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Collectivized"
-        :value="$app.site.stats.collectionItems.toLocaleString()"
+        :value="$app.site.stats.collectionItems?.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="TPU Hours"
-        :value="$app.site.stats.pulse.toLocaleString() + 'h'"
+        :value="$app.site.stats.pulse?.toLocaleString() + 'h'"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Users"
-        :value="$app.site.stats.users.toLocaleString()"
+        :value="$app.site.stats.users?.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Uploads"
-        :value="$app.site.stats.uploads.toLocaleString()"
+        :value="$app.site.stats.uploads?.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
@@ -39,37 +39,37 @@
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Pulses"
-        :value="$app.site.stats.pulses.toLocaleString()"
+        :value="$app.site.stats.pulses?.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Invited Users"
-        :value="$app.site.stats.invites.toLocaleString()"
+        :value="$app.site.stats.invites?.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Documents"
-        :value="$app.site.stats.docs.toLocaleString()"
+        :value="$app.site.stats.docs?.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Messages"
-        :value="$app.site.stats.messages.toLocaleString()"
+        :value="$app.site.stats.messages?.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Chats"
-        :value="$app.site.stats.chats.toLocaleString()"
+        :value="$app.site.stats.chats?.toLocaleString()"
       ></StatsCard>
     </v-col>
     <v-col :cols="cols" :lg="lg" :md="md" :sm="sm" :xl="xl">
       <StatsCard
         title="Crashes"
-        :value="$app.site.stats.crashes.toLocaleString()"
+        :value="$app.site.stats.crashes?.toLocaleString()"
       ></StatsCard>
     </v-col>
   </v-row>
