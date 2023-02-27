3ef6ffbb26423cbbbfd578c757d03881d80ea37b
Troplo <troplo@troplo.com>
2023-02-27 20:30:18 +1100

    Add missing associations

---
+++ app/migrations/20230226100341-emailVerificationToken.js
@@ -0,0 +1,20 @@
+"use strict"
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Users", "emailToken", {
+      type: Sequelize.STRING,
+      allowNull: true
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+}
