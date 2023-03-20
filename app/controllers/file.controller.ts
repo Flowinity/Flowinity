b58b2e9f988198028f7c5ecdd2821178fddee5b8
Troplo <troplo@troplo.com>
2023-03-20 18:31:57 +1100

    fuck you Google

--- app/controllers/file.controller.ts
+++ app/controllers/file.controller.ts
@@ -3,6 +3,8 @@ import { Service } from "typedi"
 import Errors from "@app/lib/errors"
 import { Upload } from "@app/models/upload.model"
 import * as fs from "fs"
+import auth from "@app/lib/auth"
+import { RequestAuth } from "@app/types/express"
 
 @Service()
 export class FileController {
@@ -29,61 +31,77 @@ export class FileController {
      *       200:
      *         description: OK
      */
-    this.router.get("/:attachment", async (req: Request, res: Response) => {
-      try {
-        const upload = await Upload.findOne({
-          where: {
-            attachment: req.params.attachment
-          }
-        })
-        if (config.release === "dev") {
-          try {
-            await fs.accessSync(
-              config.storage + "/" + req.params.attachment,
-              fs.constants.F_OK
-            )
-          } catch {
-            return res.redirect(
-              "https://i.troplo.com/i/" + req.params.attachment
-            )
-          }
-        }
-        if (!upload) {
-          throw Errors.NOT_FOUND
-        }
-        if (req.query.force) {
-          return res.download(
-            config.storage + "/" + req.params.attachment,
-            upload.originalFilename
-          )
-        }
-        if (
-          upload.type === "image" ||
-          upload.type === "video" ||
-          upload.type === "audio"
-        ) {
-          return res.sendFile("/" + upload.attachment, {
-            root: config.storage,
-            name: upload.originalFilename
-          })
-        } else {
-          return res.download(
-            config.storage + "/" + upload.attachment,
-            upload.originalFilename
-          )
-        }
-      } catch (err) {
-        res.status(404)
-        return res.json({
-          errors: [
-            {
-              name: "NOT_FOUND",
-              message: "The requested resource could not be found.",
-              status: 404
+    this.router.get(
+      "/:attachment",
+      auth("user.view", true),
+      async (req: RequestAuth, res: Response) => {
+        try {
+          const upload = await Upload.findOne({
+            where: {
+              attachment: req.params.attachment
             }
-          ]
-        })
+          })
+          if (config.release === "dev") {
+            try {
+              await fs.accessSync(
+                config.storage + "/" + req.params.attachment,
+                fs.constants.F_OK
+              )
+            } catch {
+              return res.redirect(
+                "https://i.troplo.com/i/" + req.params.attachment
+              )
+            }
+          }
+          if (!upload) {
+            throw Errors.NOT_FOUND
+          }
+          //Acropalypse temporary patch
+          if (
+            upload.userId === 1 &&
+            upload.name.startsWith("Screenshot_2022") &&
+            req.user?.id !== 1
+          ) {
+            return res.sendFile("/AuthRequired.png", {
+              name: upload.originalFilename,
+              root: config.storage + "/../app/assets"
+            })
+          }
+          if (req.query.force) {
+            return res.download(
+              config.storage + "/" + req.params.attachment,
+              upload.originalFilename
+            )
+          }
+          if (
+            upload.type === "image" ||
+            upload.type === "video" ||
+            upload.type === "audio"
+          ) {
+            return res.sendFile("/" + upload.attachment, {
+              root: config.storage,
+              name: upload.originalFilename
+            })
+          } else {
+            return res.download(
+              config.storage + "/" + upload.attachment,
+              upload.originalFilename
+            )
+          }
+        } catch (err) {
+          console.error(err)
+          res.status(404)
+          return res.json({
+            errors: [
+              {
+                name: "NOT_FOUND",
+                message: "The requested resource could not be found.",
+                status: 404
+              }
+            ]
+          })
+        }
       }
-    })
+    )
   }
 }
