d0f69126b764ea0e45c4386c04d3d407118a635b
Troplo <troplo@troplo.com>
2023-05-11 00:00:52 +1000

    Update experiments.ts

--- frontend/src/store/experiments.ts
+++ frontend/src/store/experiments.ts
@@ -25,9 +25,11 @@ export const useExperimentsStore = defineStore("experiments", {
       if (experiments) {
         try {
           this.experiments = JSON.parse(experiments);
-          axios.defaults.baseURL = import.meta.env.CORDOVA
-            ? `https://images.flowinity.com/api/${this.experiments.API_VERSION}`
-            : `/api/v${this.experiments.API_VERSION}`;
+          if (this.experiments.API_VERSION) {
+            axios.defaults.baseURL = import.meta.env.CORDOVA
+              ? `https://images.flowinity.com/api/${this.experiments.API_VERSION}`
+              : `/api/v${this.experiments.API_VERSION}`;
+          }
         } catch {
           //
         }
@@ -37,9 +39,11 @@ export const useExperimentsStore = defineStore("experiments", {
         ...data,
         ...JSON.parse(localStorage.getItem("experiments") || "{}")
       };
-      axios.defaults.baseURL = import.meta.env.CORDOVA
-        ? `https://images.flowinity.com/api/${this.experiments.API_VERSION}`
-        : `/api/v${this.experiments.API_VERSION}`;
+      if (this.experiments.API_VERSION) {
+        axios.defaults.baseURL = import.meta.env.CORDOVA
+          ? `https://images.flowinity.com/api/${this.experiments.API_VERSION}`
+          : `/api/v${this.experiments.API_VERSION}`;
+      }
       this.experimentsInherit = data;
       localStorage.setItem("experimentsStore", JSON.stringify(data));
     }
