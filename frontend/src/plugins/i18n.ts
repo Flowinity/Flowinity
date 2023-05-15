572ca8d2cb5cd391b20663a71642b786eb0db9cf
Troplo <troplo@troplo.com>
2023-05-15 23:45:26 +1000

    Update i18n.ts

--- frontend/src/plugins/i18n.ts
+++ frontend/src/plugins/i18n.ts
@@ -1,17 +1,17 @@
-import {createI18n} from "vue-i18n"
+import { createI18n } from "vue-i18n";
 
 // Import locales
-import enUS from "@/locales/en-US.json"
-import enGB from "@/locales/en-GB.json"
-import ru from "@/locales/ru.json"
+import en from "@/locales/en-US.json";
+import enGB from "@/locales/en-GB.json";
+import ru from "@/locales/ru.json";
 
 export default createI18n({
   legacy: true,
-  locale: "enUS",
-  fallbackLocale: "enUS",
+  locale: "en",
+  fallbackLocale: "en",
   messages: {
-    enUS,
+    en,
     enGB,
     ru
   }
-})
+});
