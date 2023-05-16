a63883c1db53e8eff4e7de5f27c8a3fee2d42ceb
Troplo <troplo@troplo.com>
2023-05-16 17:13:33 +1000

    Fix queue

--- app/lib/queue.ts
+++ app/lib/queue.ts
@@ -8,101 +8,105 @@ try {
   config = require("@app/config/tpu.json")
 } catch {}
 const cacheService = Container.get(CacheService)
-let queue = null,
-  cacheQueue = null,
-  worker = null,
-  cacheWorker = null
-if (config.finishedSetup) {
-  queue = new Queue("queue:uploads", {
-    connection: {
-      port: config.redis.port,
-      host: config.redis.host,
-      password: config.redis.password,
-      db: config.redis.db,
-      username: config.redis.username
-    },
-    defaultJobOptions: {
-      attempts: 3,
-      backoff: {
-        type: "exponential",
-        delay: 1000
-      }
-    }
-  })
 
-  cacheQueue = new Queue("queue:cache", {
-    connection: {
-      port: config.redis.port,
-      host: config.redis.host,
-      password: config.redis.password,
-      db: config.redis.db,
-      username: config.redis.username
-    },
-    defaultJobOptions: {
-      attempts: 1,
-      removeOnComplete: true,
-      removeOnFail: true,
-      backoff: {
-        type: "exponential",
-        delay: 1000
-      }
-    }
-  })
-
-  worker = new Worker(
-    "queue:uploads",
-    async (job) => {
-      await utils.postUpload(job.data)
-    },
-    {
-      // max number of jobs that can run concurrently
-      // another way is removing this option and making multiple workers like worker1, worker2, etx
-      concurrency: 3,
-      connection: {
+const queue = new Queue("queue:uploads", {
+  connection: config.finishedSetup
+    ? {
         port: config.redis.port,
         host: config.redis.host,
         password: config.redis.password,
         db: config.redis.db,
         username: config.redis.username
       }
+    : {},
+  defaultJobOptions: {
+    attempts: 3,
+    backoff: {
+      type: "exponential",
+      delay: 1000
     }
-  )
+  }
+})
 
-  cacheWorker = new Worker(
-    "queue:cache",
-    async (job) => {
-      await cacheService.resetCollectionCache(job.data)
-    },
-    {
-      // max number of jobs that can run concurrently
-      // another way is removing this option and making multiple workers like worker1, worker2, etx
-      concurrency: 3,
-      connection: {
+const cacheQueue = new Queue("queue:cache", {
+  connection: config.finishedSetup
+    ? {
         port: config.redis.port,
         host: config.redis.host,
         password: config.redis.password,
         db: config.redis.db,
         username: config.redis.username
       }
+    : {},
+  defaultJobOptions: {
+    attempts: 1,
+    removeOnComplete: true,
+    removeOnFail: true,
+    backoff: {
+      type: "exponential",
+      delay: 1000
     }
-  )
+  }
+})
 
-  worker.on("completed", (job) => {
-    console.log(`Job ${job.id} completed!`)
-  })
+const worker = new Worker(
+  "queue:uploads",
+  async (job) => {
+    await utils.postUpload(job.data)
+  },
+  {
+    // max number of jobs that can run concurrently
+    // another way is removing this option and making multiple workers like worker1, worker2, etx
+    concurrency: 3,
+    connection: config.finishedSetup
+      ? {
+          port: config.redis.port,
+          host: config.redis.host,
+          password: config.redis.password,
+          db: config.redis.db,
+          username: config.redis.username
+        }
+      : {}
+  }
+)
 
-  worker.on("failed", (job, err) => {
-    console.log(`Job ${job?.id} failed with error ${err}`)
-  })
+const cacheWorker = new Worker(
+  "queue:cache",
+  async (job) => {
+    await cacheService.resetCollectionCache(job.data)
+  },
+  {
+    // max number of jobs that can run concurrently
+    // another way is removing this option and making multiple workers like worker1, worker2, etx
+    concurrency: 3,
+    connection: config.finishedSetup
+      ? {
+          port: config.redis.port,
+          host: config.redis.host,
+          password: config.redis.password,
+          db: config.redis.db,
+          username: config.redis.username
+        }
+      : {}
+  }
+)
 
-  cacheWorker.on("completed", (job) => {
-    console.log(`Job ${job.id} completed!`)
-  })
+worker.on("completed", (job) => {
+  console.log(`Job ${job.id} completed!`)
+})
 
-  cacheWorker.on("failed", (job, err) => {
-    console.log(`Job ${job?.id} failed with error ${err}`)
-  })
-}
+worker.on("failed", (job, err) => {
+  console.log(`Job ${job?.id} failed with error ${err}`)
+})
+
+cacheWorker.on("completed", (job) => {
+  console.log(`Job ${job.id} completed!`)
+})
+
+cacheWorker.on("failed", (job, err) => {
+  console.log(`Job ${job?.id} failed with error ${err}`)
+})
+
 export default {
   queue,
   worker,
