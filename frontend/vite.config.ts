d42cb5f0338d137f0b262683cbb5c52cfb07b77d
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2024-01-05 18:40:58 +1100

    Reformat vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -138,6 +138,7 @@ const config = {
   server: {
     port: 3000,
     proxy: {
+      "/api/v1": "http://localhost:34581",
       "/api/v2": "http://localhost:34582",
       "/api/v3": "http://localhost:34582",
       "/api/v4": "http://localhost:34582",
@@ -148,7 +149,6 @@ const config = {
         secure: true,
         changeOrigin: true
       },
-      "/api/v1": "http://localhost:34581",
       "/graphql": {
         target: "http://localhost:34583",
         ws: true,
