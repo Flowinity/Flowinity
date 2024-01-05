f2ec812b76b36015503d014819b49aa3f7406912
Troplo <troplo@troplo.com>
2024-01-05 17:35:02 +1100

    Fix type

--- frontend-v5/src/boot/declarations.ts
+++ frontend-v5/src/boot/declarations.ts
@@ -76,11 +76,6 @@ declare global {
       user: any;
       emit: (platform: string, event: string, data: any) => void;
     };
-    __TROPLO_INTERNALS_EDITOR_SAVE: (args: any) => any;
-    __TROPLO_INTERNALS_UPDATE_COUNT: (args: any) => any;
-    __TROPLO_INTERNALS_EDITOR_UPLOAD: (args: any) => any;
-    __TROPLO_INTERNALS_NOTE_ID: number;
-    __NOTE_DATA: any;
   }
 
   interface navigator extends Navigator {
