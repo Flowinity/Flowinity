Flowinity/Flowinity

commit 3a30bc7529de2e8cc60aeb1c6a1f2aced9977dde

Remove APIv1

app/app.ts
@@ -72,7 +72,7 @@ import { GqlError } from "@app/lib/gqlErrors"
 @Service()
 @Middleware({ type: "after" })
 export class HttpErrorHandler implements ExpressErrorMiddlewareInterface {
-  error(err: any, req: any, res: any, next: (err: any) => any) {
+  error(err: any, res: any) {
     if (err instanceof MulterError) {
       return res.status(400).json({
         errors: [
@@ -252,27 +252,6 @@ export class Application {
     this.createExpressServerV3("/api/v3")
     this.createExpressServerV3("/api/v2")
 
-    // For clients that still use /api/v1, the schema is still the same for upload API, so we'll use v3
-    useExpressServer(this.app, {
-      controllers: config.finishedSetup
-        ? [CoreControllerV3, GalleryControllerV3]
-        : [],
-      routePrefix: "/api/v1",
-      middlewares: [HttpErrorHandler],
-      defaultErrorHandler: false,
-      classTransformer: false,
-      defaults: {
-        undefinedResultCode: 204,
-        nullResultCode: 404
-      },
-      validation: true
-    })
-
-    // OIDC
-    /*const provider = new Provider("http://localhost:34583", {
-      adapter: OidcAdapter
-    })
-    provider.listen(34583, () => {})*/
     this.app.get("/.well-known/openid-configuration", (req, res) => {
       res.json(wellKnownOidc())
     })

app/controllers/v3/chat.controller.ts
@@ -245,28 +245,6 @@ export class ChatControllerV3 {
     )
   }
 
-  @Put("/:associationId/users/:userId")
-  async updateUserRank(
-    @Auth("chats.edit") user: User,
-    @Param("associationId") associationId: number,
-    @Param("userId") userId: number,
-    @Body() body: { rank: "admin" | "member" | "owner" }
-  ) {
-    throw Errors.API_REMOVED_V2
-    const rank = await this.chatService.checkPermissions(
-      user.id,
-      associationId,
-      ChatPermissions.MANAGE_RANKS
-    )
-    if (!rank) throw Errors.PERMISSION_DENIED_RANK
-    await this.chatService.updateUserRank(
-      userId,
-      associationId,
-      body.rank,
-      user.id
-    )
-  }
-
   @Put("/:associationId/message")
   async updateMessage(
     @Auth("chats.edit") user: User,

commit cfd1d7b273c5215f9cf7487cf3d95f9f5aa7dd07

Fix critical regression caused by 3a30bc7

app/app.ts
@@ -67,11 +67,12 @@ import { MulterError } from "multer"
 import { ZodError } from "zod"
 import { generateSchema } from "@app/lib/generateSchema"
 import { GqlError } from "@app/lib/gqlErrors"
+import { Response, Request } from "express"
 
 @Service()
 @Middleware({ type: "after" })
 export class HttpErrorHandler implements ExpressErrorMiddlewareInterface {
-  error(err: any, res: any) {
+  error(err: any, _: Request, res: Response) {
     if (err instanceof MulterError) {
       return res.status(400).json({
         errors: [