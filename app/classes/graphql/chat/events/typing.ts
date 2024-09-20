aa7fd6f311e26958ad38ef948a2bcc6a2cd4c3c7
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2024-09-20 20:39:14 +1000

    Fix type for ChatTypingEvent

--- app/classes/graphql/chat/events/typing.ts
+++ app/classes/graphql/chat/events/typing.ts
@@ -1,11 +1,9 @@
-import { Field, ObjectType } from "type-graphql"
-import {
-  PartialUserFriend
-} from "@app/classes/graphql/user/partialUser"
+import { Field, Int, ObjectType } from "type-graphql"
+import { PartialUserFriend } from "@app/classes/graphql/user/partialUser"
 
 @ObjectType()
 export class ChatTypingEvent {
-  @Field()
+  @Field(() => Int)
   chatId: number
   @Field(() => PartialUserFriend)
   user: PartialUserFriend
